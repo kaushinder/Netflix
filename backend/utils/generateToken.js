088478c5115ca2e0a9d27aa57fc96a741f533c38
Netflix Deploy <deploy@netflix-clone.com>
2026-03-31 13:54:53 +0530

    Initial commit - deployment ready

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars.js";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
+
+	res.cookie("jwt-netflix", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: ENV_VARS.NODE_ENV === "production" ? "none" : "strict", // "none" for cross-origin in production
+		secure: ENV_VARS.NODE_ENV !== "development", // HTTPS only in production
+	});
+
+	return token;
+};
